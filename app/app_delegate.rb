0ec10356a401798a1be9624a0c50333f372f767e
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-12-20 20:25:03 +0900

    Stop using Nimbus CSS

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,13 +4,6 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(App.bounds)
 
-    path_prefix = NIPathForBundleResource(nil, 'css')
-    host = 'http://localhost:8888/'
-    @stylesheet_cache = NIStylesheetCache.alloc.initWithPathPrefix(path_prefix)
-    @chameleonObserver = NIChameleonObserver.alloc.initWithStylesheetCache(
-      @stylesheet_cache, host:host)
-    @chameleonObserver.watchSkinChanges
-
     main_controller = MainController.alloc.initWithNibName(nil, bundle:nil)
     @root_controller = UINavigationController.alloc.initWithRootViewController(main_controller)
     @window.rootViewController = @root_controller
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,12 +9,6 @@ class MainController < UIViewController
 
   def initWithNibName(nibNameOrNil, bundle:nibBundleOrNil)
     super
-    stylesheet_cache = App.delegate.stylesheet_cache
-    stylesheet = stylesheet_cache.stylesheetWithPath('main.css')
-    @dom = NIDOM.alloc.initWithStylesheet(stylesheet)
-    @style_observer = App.notification_center.observe(NIStylesheetDidChangeNotification, stylesheet) do |notif|
-      @dom.refresh
-    end
     @working = false
     @remaining = 25 * 60
     self.title = 'WillTimer'
@@ -34,16 +28,22 @@ class MainController < UIViewController
     navigationItem.rightBarButtonItem = @settings
 
     @task = UILabel.alloc.init.tap do |l|
-      l.text = 'UI設計'
-      l.frame = [[40, 100], [(1024-100)/2, 200]]
+      l.text = 'タスクその1'
+      l.font = UIFont.fontWithName('HiraKakuProN-W6', size:128)
+      l.frame = [[40, 100], [1024-80, 200]]
+      l.backgroundColor = UIColor.clearColor
+      l.textColor = UIColor.blackColor
+      l.adjustsFontSizeToFitWidth = true
+      l.layer.borderColor = '#cccccc'.to_color
+      l.layer.borderWidth = 3
     end
-    @dom.registerView(@task, withCSSClass:'task')
 
     @left_time = UILabel.alloc.init.tap do |l|
       l.text = "10'25\""
+      l.font = UIFont.fontWithName('Verdana-BoldItalic', size:128)
       l.frame = [[40, 424], [512, 250]]
+      l.backgroundColor = UIColor.clearColor
     end
-    @dom.registerView(@left_time, withCSSClass:'lefttime')
 
     @pause_restart_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
       b.setTitle('一時停止', forState:UIControlStateNormal)
@@ -61,11 +61,6 @@ class MainController < UIViewController
     self.view.addSubview(@left_time)
     self.view.addSubview(@pause_restart_button)
     self.view.addSubview(@interrupt_button)
-  end
-
-  def viewDidUnLoad
-    @dom.unregisterAllViews
-    @label = nil
   end
 
   def open_tasks
