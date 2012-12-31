f1d3d57c9a9e8109ddf7883d515bc7261cd11a41
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-12-31 18:42:12 +0900

    Use Teacup to create view

--- app/views/task_view.rb
+++ app/views/task_view.rb
@@ -2,26 +2,23 @@ class TaskView < UIView
   attr_accessor :task, :title, :done, :memo
 
   def initWithFrame(rect)
-    super
-    self.backgroundColor = '#333333'.to_color
+    super.tap do
+      frame_width = CGRectGetWidth(rect)
+      frame_height = CGRectGetHeight(rect)
 
-    @title = UILabel.alloc.init.tap do |l|
-      l.text = 'タスク'
-      l.font = UIFont.fontWithName('HiraKakuProN-W6', size:128)
-      l.backgroundColor = UIColor.clearColor
-      l.textColor = UIColor.whiteColor
-      l.adjustsFontSizeToFitWidth = true
+      layout(self, backgroundColor: '#333333'.to_color) {
+        @title = subview(UILabel,
+          frame: [
+            [30, 10],
+            [frame_width-(30*2), frame_height-(10*2)]
+            ],
+          text: 'タスク',
+          font: UIFont.fontWithName('HiraKakuProN-W6', size:128),
+          backgroundColor: UIColor.clearColor,
+          textColor: UIColor.whiteColor,
+          adjustsFontSizeToFitWidth: true
+        )
+      }
     end
-
-    addSubview(@title)
-
-    self
-  end
-
-  def layoutSubviews
-    @title.frame = [
-      [30, 10],
-      [frame.size.width-(30*2), frame.size.height-(10*2)]
-    ]
   end
 end
--- app/views/timer_view.rb
+++ app/views/timer_view.rb
@@ -4,59 +4,60 @@ class TimerView < UIView
   attr_accessor :remains, :digits, :start_button, :pause_button, :interrupt_button
 
   def initWithFrame(rect)
-    super
-    self.backgroundColor = '#666666'.to_color
+    super.tap do
+      frame_width = CGRectGetWidth(rect)
+      frame_height = CGRectGetHeight(rect)
 
-    @digits = UILabel.alloc.init.tap do |l|
-      l.text = '00:00'
-      l.font = UIFont.fontWithName('HelveticaNeue-CondensedBold', size:256)
-      l.textColor = UIColor.whiteColor
-      l.backgroundColor = UIColor.clearColor
+      layout(self, backgroundColor: '#666666'.to_color) {
+        @digits = subview(UILabel,
+          text: '00:00',
+          font: UIFont.fontWithName('HelveticaNeue-CondensedBold', size:256),
+          textColor: UIColor.whiteColor,
+          backgroundColor: UIColor.clearColor,
+          frame: [
+            [30, 10],
+            [frame_width-(30*2)-100, frame_height-(10*2)]
+          ]
+        )
+
+        @start_button = subview(
+          UIButton.buttonWithType(UIButtonTypeRoundedRect),
+          title: '開始',
+          frame: [
+            [frame_width-30-100, 100],
+            [80, 100]
+          ]
+        )
+
+        @pause_button = subview(
+          UIButton.buttonWithType(UIButtonTypeRoundedRect),
+          title: '一時停止',
+          hidden: true,
+          frame: [
+            [frame_width-30-100, 100],
+            [80, 100]
+          ]
+        )
+
+        @interrupt_button = subview(
+          UIButton.buttonWithType(UIButtonTypeRoundedRect),
+          title: '中止',
+          frame: [
+            [frame_width-30-100, 100 + 130],
+            [80, 100]
+          ]
+        )
+      }
+
+      observe(self, :remains) do |old_value, new_value|
+        minutes = new_value / 60
+        seconds = new_value % 60
+        @digits.text = "%02d:%02d" % minutes, seconds
+      end
     end
-    addSubview(@digits)
-
-    @start_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
-      b.setTitle('開始', forState:UIControlStateNormal)
-    end
-    addSubview(@start_button)
-
-    @pause_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
-      b.setTitle('一時停止', forState:UIControlStateNormal)
-      b.hidden = true
-    end
-    addSubview(@pause_button)
-
-    @interrupt_button = UIButton.buttonWithType(UIButtonTypeRoundedRect).tap do |b|
-      b.setTitle('中止', forState:UIControlStateNormal)
-    end
-    addSubview(@interrupt_button)
-
-    observe(self, :remains) do |old_value, new_value|
-      minutes = new_value / 60
-      seconds = new_value % 60
-      @digits.text = "%02d:%02d" % minutes, seconds
-    end
-
-    self
   end
 
   def dealloc
     unobserve_all
-  end
-
-  def layoutSubviews
-    @digits.frame = [
-      [30, 10],
-      [frame.size.width-(30*2)-100, frame.size.height-(10*2)]
-    ]
-    @start_button.frame = [
-      [frame.size.width-30-100, 100],
-      [80, 100]
-    ]
-    @pause_button.frame = @start_button.frame
-    @interrupt_button.frame = [
-      [frame.size.width-30-100, 100 + 130],
-      [80, 100]
-    ]
   end
 end
